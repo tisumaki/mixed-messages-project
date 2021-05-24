ac1f3a2360d33b3f57e06d0736d3315c35dc96a2
tisumaki <84583619+tisumaki@users.noreply.github.com>
2021-05-24 15:21:40 +0300

    Added object with list items

---
+++ script.js
@@ -0,0 +1,5 @@
+let lists = {
+    listCompliment: ['beautiful', 'wonderful', 'amazing', 'stunning', 'gorgeous', 'breathtaking'],
+    listActivity: ['go outside', 'read a book', 'relax', 'do some exercise', 'message your friend'],
+    listReminder: ['to drink enough water', 'to take care of yourself', 'you are not alone', 'to get enough sleep', 'to eat well']
+};
