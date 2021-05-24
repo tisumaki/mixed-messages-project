cc5326db7249cab80a8808abc4b440698b0d1c78
tisumaki <84583619+tisumaki@users.noreply.github.com>
2021-05-24 16:13:26 +0300

    First draft of the code with several functions

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ let lists = {
     listActivity: ['go outside', 'read a book', 'relax', 'do some exercise', 'message your friend'],
     listReminder: ['to drink enough water', 'to take care of yourself', 'you are not alone', 'to get enough sleep', 'to eat well']
 };
+
+const mixedCompliment = () => {
+    const randomCompliment = lists.listCompliment[Math.floor(Math.random() * 5)];
+    return `You look ${randomCompliment} today!`
+};
+
+const mixedActivity = () => {
+    const randomActivity = lists.listActivity[Math.floor(Math.random() * 5)];
+    return `We suggest that today you should ${randomActivity}.`
+};
+
+const mixedReminder = () => {
+    const randomReminder = lists.listReminder[Math.floor(Math.random() * 5)];
+    return `Remember ${randomReminder}!`
+};
+
+const mixedMessage = `Our message for you today is: ${mixedCompliment()} ${mixedActivity()} ${mixedReminder()}`
+console.log(mixedMessage);
